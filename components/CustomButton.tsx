18b3603d74a57e0ff871b9b227a51dff367a40f2
freetimezp <kraftukrnet@ukr.net>
2023-06-11 19:53:06 +0300

    03. js-cars-03. Navbar, Footer, Layout Components.

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -4,13 +4,13 @@ import Image from 'next/image';
 
 import { CustomButtonProps } from '@/types';
 
-const CustomButton = ({ title, containerStyles, handleClick }: CustomButtonProps) => {
+const CustomButton = ({ title, containerStyles, handleClick, btnType }: CustomButtonProps) => {
     return (
         <button
             disabled={false}
-            type={"button"}
             className={`custom-btn ${containerStyles}`}
             onClick={handleClick}
+            type={btnType || "button"}
         >
             <span className={`flex-1`}>
                 {title}
