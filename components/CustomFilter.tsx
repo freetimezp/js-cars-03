ad4532a071523975c987e58fcb88802f9022f8b1
freetimezp <kraftukrnet@ukr.net>
2023-06-12 18:56:05 +0300

    04. js-cars-03. SearchBar, SearchManufacturer Components. Show data menu items from constans Manufacturers.

---
+++ components/CustomFilter.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CustomFilter = ({ title }: any) => {
+    return (
+        <div>
+            {title}
+        </div>
+    );
+};
+
+export default CustomFilter;
--- types/index.ts
+++ types/index.ts
@@ -6,3 +6,8 @@ export interface CustomButtonProps {
     handleClick?: MouseEventHandler<HTMLButtonElement>;
     btnType?: "button" | "submit";
 }
+
+export interface SearchManufacturerProps {
+    manufacturer: String;
+    setManufacturer: (manufacturer: string) => void;
+}
