a50475b33c49c458f9d3518459a566294b1c20bc
freetimezp <kraftukrnet@ukr.net>
2023-06-14 19:00:36 +0300

    12. js-cars-03. ShowMore Component, func.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-import { Hero, SearchBar, CustomFilter, CarCard } from '../components';
+import { Hero, SearchBar, CustomFilter, CarCard, ShowMore } from '../components';
 import { fetchCars } from "@/utils";
 import { fuels, yearsOfProduction } from "@/constans";
 
@@ -40,6 +40,11 @@ export default async function Home({ searchParams }: any) {
                         <CarCard car={car} key={i} />
                      ))}
                   </div>
+
+                  <ShowMore
+                     pageNumber={(searchParams.limit || 10) / 10}
+                     isNext={(searchParams.limit || 10) > allCars.length}
+                  />
                </section>
             ) : (
                <div className="home__error-container">
